aa3283272b5a0ef684ab371996d425fad713f0ef
Manel Arrufat <manelarrufat@gmail.com>
2024-12-07 13:00:37 +0100

    Component de React

---
+++ src/components/products/ProductCard.tsx
@@ -0,0 +1,35 @@
+import type { ProductWithImages } from "@/interfaces"
+import { useState } from "react";
+
+interface Props {
+    product: ProductWithImages;
+}
+
+
+export const ProductCard = ({product}:Props) => {
+
+    const images = product.images.split(',').map(img => {
+        return img.startsWith('http')
+            ? img
+            : `${import.meta.env.PUBLIC_URL}/images/products/${img}`;
+    });
+
+    const [currentImage, setcurrentImage] = useState(images[0]);
+
+  return (
+    <a href={`/products/${product.slug}`}>
+        <img 
+            src={currentImage} 
+            alt={product.title}
+            className="h-[350px] object-contain"
+            onMouseEnter={ () => setcurrentImage(images[1] ?? images[0])}
+            onMouseLeave={ () => setcurrentImage(images[0])}
+            
+            
+
+        />
+        <h4>{product.title}</h4>
+        <p>€{product.price}</p>
+    </a>
+  )
+}
